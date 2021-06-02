f27c3ba6c12e5bf5b2494b5d832e4064b0fcd521
benoitberanger <benoit.beranger@icm-institute.org>
2021-06-03 00:56:39 +0200

    file reading works, and its slower than the MATLAB builtin strategy BUT : what about parsing the content ?

--- compile_and_run.m
+++ compile_and_run.m
@@ -5,4 +5,12 @@ mex -setup C++
 
 mex jsonstack2structcenir.cpp
 
-jsonstack2structcenir('stack_DEV_412_PROVOCAH_Pilote4_S6_Run_1.json')
+fname = 'stack_DEV_412_PROVOCAH_Pilote4_S6_Run_1.json';
+
+tic
+content = jsonstack2structcenir(fname);
+toc*1000
+
+tic
+content = get_file_content_as_char( fname );
+toc*1000
--- jsonstack2structcenir.cpp
+++ jsonstack2structcenir.cpp
@@ -12,8 +12,9 @@ using matlab::data::Array;
 // entry point of the mex
 class MexFunction : public matlab::mex::Function {
     
-    // Input stream to read words from file
-    std::ifstream inFile;
+    // File
+    std::ifstream inFile; // Input stream to read from file
+    std::string content;  // File content
     
     // Get pointer to engine
     std::shared_ptr<matlab::engine::MATLABEngine> matlabPtr = getEngine();
@@ -22,46 +23,6 @@ class MexFunction : public matlab::mex::Function {
     matlab::data::ArrayFactory factory;
     
 public:
-    
-    /*********************************************************************/
-    // Destructor
-    ~MexFunction() {
-        if (inFile.is_open())
-            inFile.close(); // make sure the file is closed at the end of execution
-    }
-    
-    /*********************************************************************/
-    void checkArguments(ArgumentList outputs, ArgumentList inputs) {
-        
-        // Check  number of inputs
-        if ( inputs.size() != 1) { error_matlab("Need exactly 1 input"); }
-        if (outputs.size()  > 1) { error_matlab("Too many outputs"    ); }
-                
-        // Check nature of input
-        if ( inputs[0].getType() != ArrayType::CHAR ) { error_matlab("Input must be a char"); }
-        
-    }
-    
-    /*********************************************************************/
-    void readFile(std::string filename){
-        inFile.open(filename);
-        std::ostringstream stringStream;
-        stringStream << "File not opened : " << filename;
-        if (!inFile.is_open()) { error_matlab(stringStream.str()); }
-    }
-    
-    /*********************************************************************/
-    // this is the main()
-    void operator()(ArgumentList outputs, ArgumentList inputs) {
-        
-        // Validate arguments
-        checkArguments(outputs, inputs);
-        
-        matlab::data::CharArray char_input = std::move(inputs[0]);
-        
-        readFile(char_input.toAscii());
-        
-    }
     
     /*********************************************************************/
     // simple wrappers
@@ -76,6 +37,62 @@ public:
         stringStream << "[" << FILENAME << "]: " << inputString;
         matlabPtr->feval(u"error", 0,
                 std::vector<Array>({ factory.createScalar( stringStream.str() ) }));
+    }
+    
+    /*********************************************************************/
+    // Destructor
+    ~MexFunction() {
+        if (inFile.is_open())
+            inFile.close(); // make sure the file is closed at the end of execution
+    }
+    
+    /*********************************************************************/
+    void checkArguments(ArgumentList outputs, ArgumentList inputs) {
+        
+        // Check  number of inputs
+        if ( inputs.size() != 1) { error_matlab("Need exactly 1 input"); }
+        if (outputs.size()  > 1) { error_matlab("Too many outputs"    ); }
+        
+        // Check nature of input
+        if ( inputs[0].getType() != ArrayType::CHAR ) { error_matlab("Input must be a char"); }
+        
+    }
+    
+    /*********************************************************************/
+    void readFile(std::string filename){
+        
+        // open or error
+        inFile.open(filename);
+        if (!inFile.is_open()) {
+            std::ostringstream stringStream;
+            stringStream << "File not opened : " << filename;
+            error_matlab(stringStream.str());
+        }
+        
+        // read file
+        std::stringstream strStream;
+        strStream << inFile.rdbuf(); //read the file
+        content = strStream.str();
+        
+        // close
+        inFile.close();
+        
+    }
+    
+    /*********************************************************************/
+    // this is the main()
+    void operator()(ArgumentList outputs, ArgumentList inputs) {
+        
+        // Validate arguments
+        checkArguments(outputs, inputs);
+        
+        const matlab::data::CharArray char_input = std::move(inputs[0]);
+        readFile(char_input.toAscii());
+        
+        // fprintf_matlab(content);
+        
+        outputs[0] = factory.createCharArray(content);
+        
     }
     
 };

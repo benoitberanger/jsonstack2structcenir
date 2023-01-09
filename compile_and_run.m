115947b77c38f20d3daacf04d0f8f59e761b7094
Benoît Béranger <benoit.beranger@icm-institute.org>
2023-01-09 17:34:23 +0100

    basic functionnaly are working

--- compile_and_run.m
+++ compile_and_run.m
@@ -1,16 +1,37 @@
 clc
 clear
 
-mex -setup C++
+% mex -setup C++
 
 mex jsonstack2structcenir.cpp
 
 fname = 'stack_DEV_412_PROVOCAH_Pilote4_S6_Run_1.json';
 
+
+%%
+
+fprintf('\n')
+
 tic
 content = jsonstack2structcenir(fname)
-toc*1000
+fprintf('jsonstack2structcenir : %g \n', toc*1000)
+
+tic
+field_to_get={
+    'RepetitionTime'
+    'EchoTime'
+    };
+field_type={
+    'double'
+    'double'
+    };
+content2 = get_string_from_json( fname , field_to_get , field_type );
+fprintf('get_string_from_json : %g \n', toc*1000)
 
 % tic
-% content = get_sequence_param_from_json( fname );
-% toc*1000
+% content3 = get_sequence_param_from_json(fname);
+% fprintf('get_sequence_param_from_json : %g \n', toc*1000)
+
+% tic
+% content4 = spm_jsonread(fname);
+% fprintf('spm_jsonread : %g \n', toc*1000)
--- jsonstack2structcenir.cpp
+++ jsonstack2structcenir.cpp
@@ -5,80 +5,70 @@
 
 #define FILENAME "jsonstack2structcenir"
 
-std::vector<std::string> field_list = {
-    "RepetitionTime",
-    "EchoTime",
-};
-
-// decimal number in fixed format (99 -99. +99.999 -.999 -3 etc.)
-// prefixed by beginning of string or white space
-// with an optional + or -
-// suffixed by end of string or white space
-std::regex num_re("(?:^|\\s)[+-]?(?:\\d+\\.?\\d*|\\d*\\.?\\d+)(?:\\s|$)*");
-
-using matlab::mex::ArgumentList;
-using matlab::data::ArrayType;
-using matlab::data::TypedArray;
-using matlab::data::Array;
-
 // entry point of the mex
 class MexFunction : public matlab::mex::Function {
     
+    protected:
+    
     // File
-    std::ifstream inFile; // Input stream to read from file
-    std::string content;  // File content
+    std::ifstream _inFile; // Input stream to read from file
+    std::string _content;  // File content
     
     // Get pointer to engine
-    std::shared_ptr<matlab::engine::MATLABEngine> matlabPtr = getEngine();
+    std::shared_ptr<matlab::engine::MATLABEngine> _matlabPtr = getEngine();
     
     // Get array factory
-    matlab::data::ArrayFactory factory;
+    matlab::data::ArrayFactory _factory;
     
-    // Create an output stream
-    std::ostringstream stream;
+    // Create an output stream, for fprintf and error
+    std::ostringstream _OUTstream;
     
-public:
+    // decimal number in fixed format (99 -99. +99.999 -.999 -3 etc.)
+    // prefixed by beginning of string or white space
+    // with an optional + or -
+    // suffixed by end of string or white space
+    std::regex _num_re{"(?:^|\\s)[+-]?(?:\\d+\\.?\\d*|\\d*\\.?\\d+)(?:\\s|$)*"};
     
     /*********************************************************************/
     // simple wrappers
-    void fprintf_matlab(const std::string inputString){
-        stream << "[" << FILENAME << "]: " << inputString;
-        matlabPtr->feval(u"fprintf", 0,
-                std::vector<Array>({ factory.createScalar( stream.str() ) }));
-                stream.str("");
+    void fprintf_matlab(const std::string & inputString){
+        _OUTstream << "[" << FILENAME << "]: " << inputString;
+        _matlabPtr->feval(u"fprintf", 0,
+                std::vector<matlab::data::Array>({ _factory.createScalar( _OUTstream.str() ) }));
+                _OUTstream.str("");
     }
-    void error_matlab(const std::string inputString){
-        stream << "[" << FILENAME << "]: " << inputString;
-        matlabPtr->feval(u"error", 0,
-                std::vector<Array>({ factory.createScalar( stream.str() ) }));
-                stream.str("");
+    void error_matlab(const std::string & inputString){
+        _OUTstream << "[" << FILENAME << "]: " << inputString << "\n";
+        _matlabPtr->feval(u"error", 0,
+                std::vector<matlab::data::Array>({ _factory.createScalar( _OUTstream.str() ) }));
+                _OUTstream.str("");
     }
     
     /*********************************************************************/
     // Destructor
     ~MexFunction() {
-        if (inFile.is_open())
-            inFile.close(); // make sure the file is closed at the end of execution
+        if (_inFile.is_open())
+            _inFile.close(); // make sure the file is closed at the end of execution
     }
     
     /*********************************************************************/
-    void checkArguments(ArgumentList outputs, ArgumentList inputs) {
+    void checkArguments(matlab::mex::ArgumentList outputs, matlab::mex::ArgumentList inputs) {
         
         // Check  number of inputs
         if ( inputs.size() != 1) { error_matlab("Need exactly 1 input"); }
         if (outputs.size()  > 1) { error_matlab("Too many outputs"    ); }
         
         // Check nature of input
-        if ( inputs[0].getType() != ArrayType::CHAR ) { error_matlab("Input must be a char"); }
+        if ( inputs[0].getType() != matlab::data::ArrayType::CHAR ) { error_matlab("Input must be a char"); }
         
     }
     
     /*********************************************************************/
-    void readFile(std::string filename){
+    void readFile(const std::string & filename){
         
         // open or error
-        inFile.open(filename);
-        if (!inFile.is_open()) {
+        _inFile.open(filename);
+        if (!_inFile.is_open()) {
             std::ostringstream stringStream;
             stringStream << "File not opened : " << filename;
             error_matlab(stringStream.str());
@@ -86,85 +76,100 @@ public:
         
         // read file
         std::stringstream strStream;
-        strStream << inFile.rdbuf(); //read the file
-        content = strStream.str();
+        strStream << _inFile.rdbuf(); //read the file
+        _content = strStream.str();
         
         // close
-        inFile.close();
-        
+        _inFile.close();
     }
     
     /*********************************************************************/
-    std::string extract_line( std::regex re ){
+    std::string extract_singleline( const std::string & str ){
         
         std::smatch match; // this will receive the matched elements
-        
-        if(std::regex_search( content, match, re )){ // search
+        if(std::regex_search( _content, match, std::regex( "\"" + str + "\": ") )){ // search
             
             // get position of the matched postion & only keep the file from this index
-            const int start = match.position();
-            std::string tmp_string = content.substr(start,content.length());
+            const int start = match.position() + match.length();
+            std::string tmp_string = _content.substr(start,_content.length());
             
             if(std::regex_search( tmp_string, match, std::regex (",") )){ // search for the comma which is an endline
                 const int stop = match.position();
-                return content.substr(start,stop); // only return the line, ready for extraction
+                return _content.substr(start,stop); // only return the line, ready for extraction
             }
             
         }
-        return std::string (""); // this an "else"
+        return std::string(""); // this an "else"
     }
     
     /*********************************************************************/
-    std::string extract_multiline( std::regex re ){
+    std::string extract_multiline( const std::string & str ){
         
         std::smatch match; // this will receive the matched elements
         
-        if(std::regex_search( content, match, re )){ // search
+        if(std::regex_search( _content, match, std::regex(str) )){ // search
             
             // get position of the matched postion & only keep the file from this index
-            const int start = match.position();
-            std::string tmp_string = content.substr(start,content.length());
+            const int start = match.position() + match.length();
+            std::string tmp_string = _content.substr(start,_content.length());
             
             if(std::regex_search( tmp_string, match, std::regex ("]") )){ // search for the comma which is an endline
                 const int stop = match.position();
-                return content.substr(start,stop); // only return the line, ready for extraction
+                return _content.substr(start,stop); // only return the line, ready for extraction
             }
             
         }
-        return std::string (""); // this an "else"
+        return std::string(""); // this an "else"
     }
     
     /*********************************************************************/
-    double extract_num( std::string str, std::regex re ){
+    double extract_num( const std::string & str ){
         
         std::smatch match;
         
-        if(std::regex_search( str, match, re )){
-            fprintf_matlab( std::to_string(match.size()) ); 
+        if(std::regex_search( str, match, _num_re )){
             return std::stod( match[0] ) ;
         }
         return NAN; // this an "else"
-        
     }
+    
+    /*********************************************************************/
+    std::string extract_str( const std::string & str ){
+        
+        if(str.size()){
+            return str.substr(1, str.size() - 2);
+        }
+        return ""; // this an "else"
+    }
+    
+    public:
     
     /*===================================================================*/
     // this is the main()
-    void operator()(ArgumentList outputs, ArgumentList inputs) {
+    
+    void operator()(matlab::mex::ArgumentList outputs, matlab::mex::ArgumentList inputs) {
         
         // Validate arguments
         checkArguments(outputs, inputs);
+        
+        std::vector<std::string> field_list = {
+            "MRAcquisitionType",
+            "RepetitionTime",
+            "EchoTime",
+        };
+        
         
         // Get filename
         const matlab::data::CharArray filename = std::move(inputs[0]);
         readFile(filename.toAscii());
         
         // Initialize output
-        matlab::data::StructArray S = factory.createStructArray({1}, field_list);
+        matlab::data::StructArray S = _factory.createStructArray({1}, field_list);
         
         // Parse file
-        
-        S[0]["RepetitionTime"] = factory.createScalar( extract_num( extract_line     ( std::regex ("RepetitionTime") ) , num_re ) );
-        S[0]["EchoTime"      ] = factory.createScalar( extract_num( extract_multiline( std::regex ("EchoTime"      ) ) , num_re ) );
+        S[0]["MRAcquisitionType"] = _factory.createScalar( extract_str( extract_singleline("MRAcquisitionType") ) );
+        S[0]["RepetitionTime"   ] = _factory.createScalar( extract_num( extract_singleline("RepetitionTime"   ) ) );
+        S[0]["EchoTime"         ] = _factory.createScalar( extract_num( extract_multiline (      "EchoTime"   ) ) );
         
         // Finalize the output
         outputs[0] = std::move(S);
@@ -172,5 +177,4 @@ public:
     }
     
 };
-
 
